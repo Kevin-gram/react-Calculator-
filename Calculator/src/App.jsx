6d56544899a0a2b2e18cd5853d1b42da2bd8601a
Kevin-gram <k.nyiringan@alustudent.com>
2024-07-30 20:16:53 +0200

    final calculator layour

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,69 @@
+export default function App() {
+  return (
+    <div className="grid grid-cols-4 h-screen w-1/2 items mx-auto my-auto">
+      <div className="font-extrabold border border-black bg-gray-500 col-span-4 text-7xl text-white flex flex-col justify-end text-end">
+        {" "}
+        0
+      </div>
+
+      <div className="font-extrabold border  border-black text-center text-3xl flex flex-col justify-center">
+        AC
+      </div>
+      <div className="font-extrabold border text-3xl border-black flex text-center flex-col justify-center">
+        +/-
+      </div>
+      <div className="font-extrabold  border text-3xl border-black text-center flex flex-col justify-center">
+        %
+      </div>
+      <div className="font-extrabold  border text-3xl border-black text-center flex flex-col justify-center bg-orange-400">
+        ÷
+      </div>
+      <div className="font-extrabold border text-3xl border-black text-center flex flex-col justify-center">
+        7
+      </div>
+      <div className="font-extrabold   border text-3xl border-black text-center flex flex-col justify-center">
+        8
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center">
+        9
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center bg-orange-400">
+        X
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center">
+        4
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center">
+        5
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center">
+        6
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center bg-orange-400">
+        -
+      </div>
+      <div className="font-extrabold text-3xl text-black border border-black text-center flex flex-col justify-center ">
+        1
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center">
+        2
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center">
+        3
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center bg-orange-400">
+        +
+      </div>
+
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center col-span-2">
+        0
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center">
+        .
+      </div>
+      <div className="font-extrabold text-3xl border border-black text-center flex flex-col justify-center bg-orange-400">
+        =
+      </div>
+    </div>
+  );
+}
